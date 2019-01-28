3c2227e47228c04c89e83fcdf27c255db07a7a0f
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-01-28 11:11:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print('welcome')
 print('please input command')
 while True:
   command = input('>> ')
-  if command[0] == 'help':
+  if command[:4] == 'help':
     print('===========help menu===========')
     print('download [type] [releases]')
   elif command[0] == 'download':
